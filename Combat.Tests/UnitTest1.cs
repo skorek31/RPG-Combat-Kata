20dc36fcbb4302b86ec70f2075318bd1c4a08644
Łukasz Skorupski <lukasz9491@gmail.com>
2019-06-11 19:50:31 +0200

    Iteration 3

--- Combat.Tests/UnitTest1.cs
+++ Combat.Tests/UnitTest1.cs
@@ -1,14 +1,147 @@
 using System;
 using Xunit;
+using Combat;
 
 namespace Combat.Tests
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void New_Warrior_Should_Have_1000_Health()
         {
+            var testObject = new Warrior("Melee", 1);
+            Assert.Equal(1000, testObject.GetHealth());
+        }
+        
+        [Fact]
+        public void New_Warrior_Should_Have_1st_Level()
+        {
+            var testObject = new Warrior("Melee", 1);
+            Assert.Equal(1, testObject.GetLevel()); 
+        }
 
+        [Fact]
+        public void New_Warrior_ShouldBe_Alive()
+        {
+            var testObject = new Warrior("Melee", 1);
+            Assert.True(testObject.IsAlive());
+        }
+
+        [Fact]
+        public void After_Attack_Warrior_Should_Have_Less_Than_1000HP()
+        {
+            var testAttacker = new Warrior("Melee", 1, 0, 0);
+            var testTarget = new Warrior("Melee", 1, 0, 1);
+            int hpBeforeAttack = testTarget.GetHealth();
+            testAttacker.Attack(testTarget);
+            Assert.True(testTarget.GetHealth() < hpBeforeAttack);
+        }
+
+        [Fact]
+        public void Warriors_Attack_Dmg_Between_50_and_100()
+        {
+            var testAttacker = new Warrior("Melee", 1, 0, 0);
+            var testTarget = new Warrior("Melee", 1, 0, 1);
+            // int hpBeforeAttack = testTarget.GetHealth();
+            // testAttacker.Attack(testTarget);
+            // int hpAfterAttack = testTarget.GetHealth();
+            // int result = hpBeforeAttack - hpAfterAttack;
+            int damage = testAttacker.Attack(testTarget);
+            Assert.True(damage > 49 && damage < 101);
+        }
+
+        [Fact]
+        public void After_20_Hits_Warrior_Has_0HPs()
+        {
+            var testAttacker = new Warrior("Melee", 1, 0, 0);
+            var testTarget = new Warrior("Melee", 1, 0, 1);
+            for (int i = 0; i < 20; i++)
+            {
+                testAttacker.Attack(testTarget);
+            }
+            Assert.Equal(0, testTarget.GetHealth());
+        }
+
+        [Fact]
+        public void After_20_Hits_Warrior_Dies()
+        {
+            var testAttacker = new Warrior("Melee", 1, 0, 0);
+            var testTarget = new Warrior("Melee", 1, 0, 1);
+            for (int i = 0; i < 20; i++)
+            {
+                testAttacker.Attack(testTarget);
+            }
+            Assert.False(testTarget.IsAlive());
+        }
+        
+        [Fact]
+        public void Warrior_Cannot_Be_Healed_Above_1000HP()
+        {
+            var testObject = new Warrior("Melee", 1);
+            testObject.Heal();
+            Assert.Equal(1000, testObject.GetHealth());
+        }
+
+        [Fact]
+        public void Dead_Warrior_Cannot_Be_Healed()
+        {
+            var testAttacker = new Warrior("Melee", 1, 0, 0);
+            var testTarget = new Warrior("Melee", 1, 0, 1);
+            for (int i = 0; i < 20; i++)
+            {
+                testAttacker.Attack(testTarget);
+            }
+            testTarget.Heal();
+            Assert.Equal(0, testTarget.GetHealth());
+        }
+        
+        [Fact]
+        public void Warrior_Cannot_Damage_Itself()
+        {
+            var testObject = new Warrior("Melee", 1);
+            Assert.Equal(-1, testObject.Attack(testObject));
+        }
+        
+        [Fact]
+        public void Melee_Attack_OutOf_Range()
+        {
+            var testAttacker = new Warrior("Melee", 1, 0, 10);
+            var testTarget = new Warrior("Melee", 1, 0, 5);
+            Assert.Equal(-1, testAttacker.Attack(testTarget)); 
+        }
+
+        [Fact]
+        public void Melee_Attack_In_Range()
+        {
+            var testAttacker = new Warrior("Melee", 1, 0, 0);
+            var testTarget = new Warrior("Melee", 1, 0, 1);
+            Assert.Equal(0, testAttacker.Attack(testTarget));
+        }
+
+        [Fact]
+        public void Ranged_Attack_OutOf_Range()
+        {
+            var testAttacker = new Warrior("Ranged", 1, 0, 0);
+            var testTarget = new Warrior("Melee", 1, 0, 30);
+            Assert.Equal(-1, testAttacker.Attack(testTarget));
+        }
+
+        [Fact]
+        public void Ranged_Attack_In_Range()
+        {
+            var testAttacker = new Warrior("Ranged", 1, 0, 0);
+            var testTarget = new Warrior("Melee", 1, 0, 10);
+            Assert.Equal(0, testAttacker.Attack(testTarget));
+        }
+
+        [Fact]
+        public void Less_Damage_If_Target_Is_5lvl_Higher()
+        {
+            var testAttackerEqualLevel = new Warrior("Melee", 1, 0, 0);
+            var testTargetEqualLevel = new Warrior("Melee", 1, 0, 1);
+
+            var testAttackerFirstLevel = new Warrior("Melee", 1, 0, 0);
+            var testTargetSixthLevel = new Warrior("Melee", 6, 0, 0);
         }
     }
 }
--- Combat/Program.cs
+++ Combat/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+
 
 namespace Combat
 {
@@ -6,7 +8,170 @@ namespace Combat
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+        }
+    }
+
+    public interface ICharacter
+    {
+        int GetHealth();
+        int GetLevel();
+        bool IsAlive();
+    }
+
+    public class Warrior : ICharacter
+    {
+        private int health = 1000;
+        private int level = 1;
+        private bool alive = true;
+        private string characterClass;
+        private int range;
+        private int x;
+        private int y;
+        // Set random character position
+        // private static Random placement = new Random();
+        // private int x = placement.Next(0, 101);
+        // private int y = placement.Next(0, 101);
+
+
+        public Warrior(string characterClass, int level)
+        {
+            this.characterClass = characterClass;
+            this.level = level;
+
+            Random placement = new Random();
+            this.x = placement.Next(0, 101);
+            this.y = placement.Next(0, 101);
+
+            if (characterClass == "Melee")
+            {
+                this.range = 2;
+            }
+            else if (characterClass == "Ranged")
+            {
+                this.range = 20;
+            }
+        }
+
+        public Warrior(string characterClass, int level, int x, int y)
+        {
+            this.characterClass = characterClass;
+            this.x = x;
+            this.y = y;
+            this.level = level;
+
+            if (characterClass == "Melee")
+            {
+                this.range = 2;
+            }
+            else if (characterClass == "Ranged")
+            {
+                this.range = 20;
+            }
+        }
+
+        public int GetHealth()
+        {
+            return health;
+        }
+        
+        public int GetLevel()
+        {
+            return level;
+        }
+
+        public bool IsAlive()
+        {
+            return alive;
+        }
+        
+        public int Attack(Warrior target)
+        {
+            /// <summary>
+            /// Method handles warrior attack
+            /// </summary>
+            /// <param name="target">Attack target.</param>
+            /// <returns>
+            /// dmg value - if target was attacked successfully
+            /// -1 - if character tried to attack itself or is out of range
+            /// </returns>
+
+            if (target != this && this.InRange(target))
+            {
+                Random randomDamage = new Random();
+                int damageValue = -1;
+                if (target.GetLevel() >= this.level + 5)
+                {
+                    damageValue = (int)(randomDamage.Next(50, 101) * 1.5);
+                    target.health -= damageValue;
+                }
+                else if (this.level >= target.GetLevel() + 5)
+                {
+                    damageValue = (int)(randomDamage.Next(50, 101) * 0.5);
+                    target.health -= damageValue;
+                }
+                else
+                {
+                    damageValue = randomDamage.Next(50, 101);
+                    target.health -= damageValue;
+                }
+                
+                if (target.health <= 0)
+                {
+                    target.health = 0;
+                    target.alive = false;
+                    damageValue = 0;
+                }
+                return damageValue;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+        
+        public void Heal()
+        {
+            Random healValue = new Random();
+            if (alive)
+            {
+                health += healValue.Next(50, 101);
+                if (health > 1000)
+                {
+                    health = 1000;
+                }
+            }
+        }
+
+        public List<int> GetCoordinates()
+        {
+            var coordinates = new List<int>();
+            coordinates.Add(this.x);
+            coordinates.Add(this.y);
+            return coordinates;
+        }
+
+        public bool InRange(Warrior target)
+        {
+            var attackerCoordinates = this.GetCoordinates();
+            var targetCoordinates = target.GetCoordinates();
+
+            int attackerX = attackerCoordinates[0];
+            int attackerY = attackerCoordinates[1];
+
+            int targetX = targetCoordinates[0];
+            int targetY = targetCoordinates[1];
+
+            double distance = Math.Sqrt(Math.Pow((targetY - attackerY), 2) + Math.Pow((targetX - attackerX), 2));
+            
+            if (distance <= this.range)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
